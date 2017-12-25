bc64a78f4956718aa63ff66a4dc50b2d48341ac7
fengpy <feng_py@126.com>
2017-12-25 12:01:07 +0800

    add a test_cpp

---
+++ test_cpp.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+//#include <thread>
+//using namespace std;
+
+class Time_app
+{
+public:
+	void set_time(void);
+	void show_time(void);
+private:
+	int hour;
+	int minute;
+	int sec;
+};
+
+void Time_app::set_time(void)
+{
+	;
+}
+
+void Time_app::show_time(void)
+{
+	hour = 1;
+	minute = 2;
+	sec = 3;
+
+	std::cout << hour << ":" << minute << ":" << sec <<std::endl;
+}
+
+int main(int argc, char **argv)
+{
+	//std::cout << "hello world\n";
+	Time_app t1;
+
+	t1.show_time();
+	return 0;
+}
+
